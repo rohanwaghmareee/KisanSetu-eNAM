ef9d613eeaef855e8fab9900185c4601df183b7f
rohanwaghmareee <opencode@hackathon.dev>
2026-08-23 21:17:51 +0530

    chore: enable static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['localhost'],
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
