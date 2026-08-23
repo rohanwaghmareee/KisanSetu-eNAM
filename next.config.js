28985ae4ed2e9455154855fb48fb6056b6b8aeeb
rohanwaghmareee <opencode@hackathon.dev>
2026-08-23 21:19:19 +0530

    fix: basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/KisanSetu-eNAM',
+  assetPrefix: '/KisanSetu-eNAM/',
   images: {
     unoptimized: true,
     domains: ['localhost'],
